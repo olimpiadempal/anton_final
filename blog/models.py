1919340436aab362ca8e2298329b94152a472377
olimpiadempal <olimpiadempal@gmail.com>
2020-07-07 18:22:16 +0200

    Bild hochladen

---
+++ blog/models.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+   
+    image = models.ImageField(null=True, upload_to='images/') 
+
+    CATEGORY1 = 'Essen'
+    CATEGORY2 = 'Körper'
+    CATEGORY3 = 'Lifestyle'
+    CATEGORY4 = 'Sport'
+    CATEGORY5 = 'Anders'
+    CATEGORY_CHOICES = [
+        (CATEGORY1, 'Essen'),
+        (CATEGORY2, 'Körper'),
+        (CATEGORY3, 'Lifestyle'),
+        (CATEGORY4, 'Sport'),
+        (CATEGORY5, 'Anders')
+    ]
+    category = models.CharField(
+        blank=True,
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+    )
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ewa/Projekt/anton/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
